193004d4ee4c42339b6d7ec312a1d33a3471beab
Drew Atkinson <drewatkinson5@gmail.com>
2016-07-18 14:48:31 -0400

    part 5: add tests

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
 from __future__ import unicode_literals
+from django.utils import timezone
+import datetime
 
 from django.db import models
 
@@ -11,7 +13,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
